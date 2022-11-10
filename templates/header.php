3f4fb6f076aa1e87af1774cf1e0360d5c14fcf99
jisse Reitsma <jisse@yireo.com>
2022-11-10 07:57:04 +0100

    Fix padding

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header class="mb-8 flex flex-wrap items-center border-b border-gray-400">
     <a href="/" class="inline-flex pt-4 md:py-8 xs:w-2/3  mx-auto md:mx-0">
         <span class="max-w-xs"><?php include __DIR__ . '/header/logo.php' ?></span>
-        <h1 class="pt-2 text-2xl md:text-4xl font-extrabold">Mage-OS Nederland</h1>
+        <h1 class="md:pt-2 text-3xl md:text-4xl font-extrabold">Mage-OS Nederland</h1>
     </a>
 
     <nav class="inline-flex w-full md:flex-grow md:w-auto">

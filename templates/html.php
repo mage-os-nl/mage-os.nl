2c64630e37ef6b4febf82f49eb3bddf8cb34e835
Jisse Reitsma <jisse@yireo.com>
2022-11-23 19:33:37 +0100

    Add hash to CSS

--- templates/html.php
+++ templates/html.php
@@ -17,7 +17,7 @@
     <meta name="twitter:image" content="//<?= $_SERVER['HTTP_HOST'] ?>/images/mage-os.png" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/style.css?v=<?= md5_file(__DIR__.'/../pub/style.css') ?>">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">

a47cb80d2379535db382e01a0d12cd79197e06ff
Jisse Reitsma <jisse@yireo.com>
2023-01-10 15:15:12 +0100

    Add pagination to export

--- lib/EventbriteExport.php
+++ lib/EventbriteExport.php
@@ -6,33 +6,42 @@ use jamiehollern\eventbrite\Eventbrite;
 
 class EventbriteExport
 {
-    public function getAttendeesByEventId(string $eventId = '')
+    public function getAttendeesByEventId(string $eventId = '', string $continuationToken = '')
     {
         $eventbrite = new Eventbrite($this->getToken());
         if (false === $eventbrite->canConnect()) {
             return [];
         }
 
-        $response = $eventbrite->makeRequest('get', 'events/' . $eventId . '/attendees/?status=attending');
+        $endPoint = 'events/' . $eventId . '/attendees/?status=attending';
+        if ($continuationToken) {
+            $endPoint .= '&continuation='.$continuationToken;
+        }
+
+        $response = $eventbrite->makeRequest('get', $endPoint);
         $data = $response['body'];
 
-        $attendees = [];
-        $totalQty = [];
+        $export = [];
         foreach ($data['attendees'] as $attendee) {
-            $ticket = $attendee['ticket_class_name'];
+            $ticketName = $attendee['ticket_class_name'];
             $email = $attendee['profile']['email'];
-            $totalQty[$ticket] += $attendee['quantity'];
-            $attendees[$ticket]['attendees'][$email] = [
+            $export['tickets'][$ticketName][$email] = [
                 'name' => $attendee['profile']['name'],
                 'email' => $email,
                 'qty' => $attendee['quantity'],
             ];
-
         }
 
-        $attendees['total_qty'] = $totalQty;
+        if ($data['pagination']['has_more_items'] > 0) {
+            $continuationToken = $data['pagination']['continuation'];
+            $export = array_merge_recursive($export, $this->getAttendeesByEventId($eventId, $continuationToken));
+        }
 
-        return $attendees;
+        foreach (array_keys($export['tickets']) as $ticketName) {
+            $export['totals'][$ticketName] = count($export['tickets'][$ticketName]);
+        }
+
+        return $export;
     }
 
     private function getToken(): string

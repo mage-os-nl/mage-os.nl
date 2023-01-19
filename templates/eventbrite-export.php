44b4d2f161dbbe1e46cbc762b2b250b1ee7b7979
Jisse Reitsma <jisse@yireo.com>
2023-01-19 14:28:39 +0100

    Add number

--- templates/eventbrite-export.php
+++ templates/eventbrite-export.php
@@ -3,18 +3,22 @@
 <table width="100%">
     <thead>
     <tr>
+        <th>#</th>
         <th>Name</th>
         <th>Email</th>
         <th>Qty</th>
     </tr>
     </thead>
     <tbody>
+    <?php $i = 1; ?>
     <?php foreach($tickets as $ticket) : ?>
     <tr>
+        <td><?= $i ?></td>
         <td><?= $ticket['name'] ?></td>
         <td><?= $ticket['email'] ?></td>
         <td><?= $ticket['qty'] ?></td>
     </tr>
+    <?php $i++; ?>
     <?php endforeach; ?>
     </tbody>
 </table>

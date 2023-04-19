56ec9120052584d61f07e0243c322850fafa9869
jisse Reitsma <jisse@yireo.com>
2023-04-19 18:59:03 +0200

    New social image

--- templates/html.php
+++ templates/html.php
@@ -7,14 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:title" content="Mage-OS Nederland" />
     <meta property="og:description" content="Vereniging voor de Nederlandse Magento community" />
-    <meta property="og:image" content="//<?= $_SERVER['HTTP_HOST'] ?>/images/mage-os.png" />
+    <meta property="og:image" content="//<?= $_SERVER['HTTP_HOST'] ?>/images/mage-os-social.png" />
     <meta property="og:type" content="website" />
     <meta property="og:locale" content="nl_NL" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@mage_os_nl" />
     <meta name="twitter:title" content="Mage-OS Nederland" />
     <meta name="twitter:description" content="Vereniging voor de Nederlandse Magento community" />
-    <meta name="twitter:image" content="//<?= $_SERVER['HTTP_HOST'] ?>/images/mage-os.png" />
+    <meta name="twitter:image" content="//<?= $_SERVER['HTTP_HOST'] ?>/images/mage-os-social.png" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="/style.css?v=<?= md5_file(__DIR__.'/../pub/style.css') ?>">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">

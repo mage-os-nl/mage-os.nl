db61293fe69c1d5d99381a0ba8afd8383ad8e313
Jisse Reitsma <jisse@yireo.com>
2023-10-23 15:44:23 +0200

    Fix PHP version

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ yarn build
 git commit -m 'New CSS' pub/style.css
 git push origin main
 ssh yireo-php "cd /home/yireo/public_html/mage-os.nl && git pull origin main && composer install"
-yireo opcache:refresh 8.1
+yireo opcache:refresh 8.2

3d82710dd0551310bf63dcfbfbfc49d06d0258e9
Jisse Reitsma <jisse@yireo.com>
2024-01-10 20:25:42 +0100

    todos

--- bin/directory-validation.php
+++ bin/directory-validation.php
@@ -8,7 +8,9 @@ require_once '../lib/functions.php';
 
 $items = (new DirectoryProvider())->getDirectoryItems();
 foreach ($items as $item) {
+    // @todo: Check if role is set something common
     if ($item->getUrl()) {
-
+        // @todo: If a local image, make sure it exists
+        // @todo: If a remote image, make sure it does not generate a 404 error
     }
 }

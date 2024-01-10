43a27300e37a156269660435eb30e5f7e69e8c1a
Jisse Reitsma <jisse@yireo.com>
2024-01-10 20:23:17 +0100

    Fix header

--- templates/header.php
+++ templates/header.php
@@ -3,6 +3,7 @@
 use MageOsNl\Registry;
 use MageOsNl\Website\MenuItem;
 use MageOsNl\Website\Router;
+use MageOsNl\Website\Url;
 
 /** @var MenuItem[] $menuItems */
 $menuItems = include Registry::getInstance()->getContentDirectory(). '/data/topmenu.php';
@@ -16,8 +17,9 @@ $menuItems = include Registry::getInstance()->getContentDirectory(). '/data/topm
         <div>
             <a href="/">
                 <h1 class="hidden md:block pt-2 pb-2 text-3xl font-extrabold"><?= __('Mage-OS Nederland') ?></h1>
-                <h1 class="block md:hidden pb-2 text-2xl font-extrabold">Mage-OS NL</h1>
-                <h2><?= __('De Nederlandse Magento community') ?></h2>
+                <h1 class="block md:hidden text-2xl font-extrabold">Mage-OS NL</h1>
+                <h2 class="hidden md:block"><?= __('De Nederlandse Magento community') ?></h2>
+                <h2 class="block md:hidden"><?= __('Magento in Nederland') ?></h2>
             </a>
         </div>
 
@@ -46,22 +48,29 @@ $menuItems = include Registry::getInstance()->getContentDirectory(). '/data/topm
         </ul>
     </nav>
 
-    <nav class="inline-flex w-full md:flex-grow md:w-auto">
-        <ul class="flex flex-row w-full md:w-auto md:ml-auto justify-between gap-2 xl:gap-4 md:text-lg">
-            <?php foreach ($menuItems as $menuItem): ?>
-                <li class="flex-auto hidden lg:block">
-                    <a class="<?= $menuItem->getClass() ?>"
-                       href="<?= $menuItem->getUrl() ?>"><?= $menuItem->getLabel() ?></a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </nav>
-
     <div class="absolute right-0 bottom-0 text-orange-600">
         <?php if (isset($_GET['language']) && $_GET['language'] === 'en'): ?>
-            <a href="/nl<?= $_SERVER['REQUEST_URI'] ?>" title="Dutch language">NL</a> &nbsp; | &nbsp; <strong>EN</strong>
+            <a href="/nl<?= $_SERVER['REQUEST_URI'] ?>" title="Dutch language">NL</a><span class="hidden md:inline pl-2">| EN</span>
         <?php else: ?>
-            <strong>NL</strong> &nbsp; | &nbsp; <a href="/en<?= $_SERVER['REQUEST_URI'] ?>" title="English language">EN</a>
+            <span class="hidden md:inline pr-2">NL |</span><a href="/en<?= $_SERVER['REQUEST_URI'] ?>" title="English language">EN</a>
         <?php endif; ?>
     </div>
 </header>
+
+<nav class="flex w-fullmd:w-auto">
+    <div class="float-right">
+        <a class="block py-1 hover:text-primary p-2 text-white bg-orange-600 transition-colors text-center"
+           href="<?= (new Url('lid-worden'))->getUrl() ?>">
+            <?= __('Become member') ?>
+        </a>
+    </div>
+    <ul class="flex flex-row w-full md:w-auto md:ml-auto justify-between gap-2 xl:gap-4 md:text-lg">
+        <?php foreach ($menuItems as $menuItem): ?>
+            <li class="flex-auto hidden lg:block">
+                <a class="<?= $menuItem->getClass() ?>" href="<?= $menuItem->getUrl() ?>">
+                    <?= $menuItem->getLabel() ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>

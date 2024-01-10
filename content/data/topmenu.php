cc90023a2fa660b48cfe5b3c156d5b5790353d95
Jisse Reitsma <jisse@yireo.com>
2024-01-10 20:23:26 +0100

    Fix header

--- content/data/topmenu.php
+++ content/data/topmenu.php
@@ -7,8 +7,5 @@ return [
     new MenuItem('Newsletter', 'http://eepurl.com/iusMg2'),
     new MenuItem('Agenda', '/agenda'),
     new MenuItem('Blog', '/blog'),
-    new MenuItem('FAQ', '/faq'),
-    new MenuItem('Become member', '/lid-worden',
-        'block py-1 hover:text-primary p-2 text-white bg-orange-600 transition-colors text-center'
-    ),
+    new MenuItem('FAQ', '/faq')
 ];

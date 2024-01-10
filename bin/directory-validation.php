8fb9176be6e89a94925f8ee912cc289a967beeaa
Jisse Reitsma <jisse@yireo.com>
2024-01-10 20:27:27 +0100

    Exit codes

--- bin/directory-validation.php
+++ bin/directory-validation.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 use MageOsNl\Website\DirectoryProvider;
 
-require_once '../vendor/autoload.php';
-require_once '../lib/functions.php';
+require_once __DIR__ .'/../vendor/autoload.php';
+require_once __DIR__.'/../lib/functions.php';
 
 $items = (new DirectoryProvider())->getDirectoryItems();
 foreach ($items as $item) {
@@ -14,3 +14,7 @@ foreach ($items as $item) {
         // @todo: If a remote image, make sure it does not generate a 404 error
     }
 }
+
+// @todo: Implement exit(1)
+
+exit(0);

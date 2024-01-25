fe1c6917411079e5f93e64d8c79c5f76933c9914
jisse Reitsma <jisse@yireo.com>
2024-01-25 11:55:45 +0100

    Add checks at deploy time

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,9 @@
 yarn build
 git commit -m 'New CSS' pub/style.css
 git push origin main
+
+docker-compose up -d
+docker-compose exec -u www-data www bash -c "php bin/directory-validation.php" || exit
+
 ssh yireo-php "cd /home/yireo/public_html/mage-os.nl && git pull origin main && composer install"
 yireo opcache:refresh 8.2

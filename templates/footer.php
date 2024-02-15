5c25bba3adad348cce861cb84f1dd9c93d045e18
grimlink <sean.grimlink@gmail.com>
2024-02-15 13:48:59 +0100

    ADD: proper padding and spacing

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types=1);
 ?>
 <div class="bg-orange-500 text-white text-sm">
-    <footer class="mx-auto md:max-w-6xl py-16">
-        <p class="pt-4">Mage OS Nederland - Vereniging - KvK 88186288 - <a
+    <footer class="mx-auto md:max-w-6xl py-8 px-4 md:py-16 space-y-4">
+        <p>Mage OS Nederland - Vereniging - KvK 88186288 - <a
                 href="https://github.com/yireo/mage-os.nl">GitHub source</a></p>
-        <p class="pt-4">Zie je een foutje? Stuur een mail naar <a
+        <p>Zie je een foutje? Stuur een mail naar <a
                 href="mailto:bestuur@nl.mage-os.org">bestuur@nl.mage-os.org</a> of maak een Pull Request aan op onze <a
                 href="https://github.com/yireo/mage-os.nl">GitHub repository</a></p>
-        <p class="pt-4">Magento™ is a registered trademark of Adobe Inc. Mage-OS is not
+        <p>Magento™ is a registered trademark of Adobe Inc. Mage-OS is not
             affiliated with Adobe or Magento Open Source in any way.</p>
     </footer>
 </div>

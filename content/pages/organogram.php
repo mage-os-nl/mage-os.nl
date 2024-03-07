e94e23e468507123ae4971b941a24c12decda7f3
Jisse Reitsma <jisse@yireo.com>
2024-03-07 08:58:10 +0100

    Pending changes

---
+++ content/pages/organogram.php
@@ -0,0 +1,66 @@
+<?php
+
+?>
+<svg
+    xmlns:svg="http://www.w3.org/2000/svg"
+    xmlns="http://www.w3.org/2000/svg"
+    width="600px"
+    height="300px"
+    version="1.0">
+
+    <!-- Top box -->
+    <rect
+        style="fill:#ccc;"
+        id="box-top"
+        width="100"
+        height="60"
+        x="250"
+        y="50"
+        rx="0"
+        ry="0" />
+    <text x="300" y="80" text-anchor="middle" alignment-baseline="middle">
+        Board
+    </text>
+    <path
+        style="stroke:#000000;stroke-width:2"
+        d="M 300,110 L 300,130"
+        id="box-line-top" />
+
+    <path style="stroke:#000000;stroke-width:2;"
+          d="M 200,130 L 400,130"
+          id="horizontal-bar" />
+
+    <!-- Box left -->
+    <rect
+        style="fill:#ccc;"
+        id="box-left"
+        width="100"
+        height="60"
+        x="150"
+        y="150"
+        rx="0"
+        ry="0" />
+    <path
+        style="stroke:#000000;stroke-width:2;"
+        d="M 200,130 L 200,150"
+        id="box-line-left" />
+
+    <!-- Box right -->
+    <rect
+        style="fill:#ccc"
+        id="box-right"
+        width="100"
+        height="60"
+        x="350"
+        y="150"
+        rx="0"
+        ry="0" />
+    <path
+        style="stroke:#000000;stroke-width:2;"
+        d="M 400,130 L 400,150"
+        id="box-line-left" />
+
+    <text x="300" y="200" text-anchor="middle" alignment-baseline="middle">
+        Committees
+    </text>
+</svg>

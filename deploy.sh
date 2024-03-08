0c607069c77cddd85914ffb1aee39e00f7b6df46
Jisse Reitsma <jisse@yireo.com>
2024-03-08 15:51:41 +0100

    Update composer; change deploy user

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git push origin main
 docker-compose up -d
 docker-compose exec -u www-data www bash -c "php bin/directory-validation.php" || exit
 
-ssh yireo-php "cd /home/mageos/public_html/mage-os.nl && ./cron.sh"
+ssh mageos "cd /home/mageos/public_html/mage-os.nl && ./cron.sh"

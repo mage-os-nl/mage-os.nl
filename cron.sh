9f42f3a35c1396dc7351470f64d6586a93e1c6de
Yireo PHP Server <info@yireo.com>
2024-03-08 15:36:51 +0100

    Change socket

--- cron.sh
+++ cron.sh
@@ -3,4 +3,4 @@ php bin/directory-validation.php || exit
 
 git pull origin main
 composer install --optimize-autoloader --no-dev
-/usr/local/bin/cachetool opcache:reset --fcgi=/var/run/php/php8.2-fpm-yireo.sock
+/usr/local/bin/cachetool opcache:reset --fcgi=/var/run/php/php8.2-fpm-mageos.sock

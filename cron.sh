31956842c7b12ca167e71e2bc1eb04d2501d7694
Jisse Reitsma <jisse@yireo.com>
2024-03-08 15:49:17 +0100

    Improve cronjob and deployment

--- cron.sh
+++ cron.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+git diff main...origin/main --quiet --exit-code && exit
+
 php bin/directory-validation.php || exit
 
 git pull origin main
+
 composer install --optimize-autoloader --no-dev
+
 /usr/local/bin/cachetool opcache:reset --fcgi=/var/run/php/php8.2-fpm-mageos.sock
--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,4 @@ git push origin main
 docker-compose up -d
 docker-compose exec -u www-data www bash -c "php bin/directory-validation.php" || exit
 
-ssh yireo-php "cd /home/yireo/public_html/mage-os.nl && git pull origin main && composer install"
-yireo opcache:refresh 8.2
+ssh yireo-php "cd /home/yireo/public_html/mage-os.nl && ./cron.sh"

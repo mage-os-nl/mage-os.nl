823e19a1fd51ed3ce4e281738edd538f07ca39fb
Jisse Reitsma <jisse@yireo.com>
2025-07-06 11:04:32 +0200

    PHP 8.3

--- cron.sh
+++ cron.sh
@@ -7,4 +7,4 @@ git pull origin main
 
 composer install --optimize-autoloader --no-dev
 
-/usr/local/bin/cachetool opcache:reset --fcgi=/var/run/php/php8.2-fpm-mageos.sock
+/usr/local/bin/cachetool opcache:reset --fcgi=/var/run/php/php8.3-fpm-mageos.sock

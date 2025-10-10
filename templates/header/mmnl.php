7270075cc706e94efcabf2c21af6213c7646775e
Lars <lars@madebymouses.com>
2025-10-10 17:18:11 +0200

    Show MMNL header to get tickets

--- templates/header/mmnl.php
+++ templates/header/mmnl.php
@@ -3,13 +3,32 @@ declare(strict_types=1);
 
 use MageOsNl\Website\ImageFactory;
 
+$image = ImageFactory::create('headers/mosnl-meeting-2024-02.jpg', 'Become a member');
+$image->setCssClass('w-full h-full object-cover');
+
+if (strstr($_SERVER['REQUEST_URI'], 'lid-worden')) {
+    return;
+}
 ?>
-<div class="pb-8 relative">
-    <a href="https://nl.meet-magento.com" title="Meet Magento Nederland 2024" target="_new">
-        <div class="absolute pt-8 md:top-1/4 left-1/4 md:left-1/3 m-auto pl-4 md:pl-24">
-            <h2 class="text-3xl md:text-7xl text-purple-950	 font-obviously-super leading-none uppercase">Meet Magento<br/>Netherlands</h2>
-            <h3 class="text-2xl md:text-5xl text-orange-600 font-obviously-condensed uppercase">October 31st, 2024 - LIEF Amsterdam</h3>
+<div class="py-8 md:py-32 relative" id="banner-wwvd">
+    <div class="absolute top-0 left-0 right-0 bottom-0">
+        <?= $image ?>
+    </div>
+    <div class="relative flex items-center justify-center">
+        <div class="text-center bg-green px-8 py-16 rounded-xl">
+            <h2 class="text-1xl md:text-3xl sm:text-5xl text-orange font-extrabold uppercase text-center">
+                Meet Magento Netherlands 2025
+            </h2>
+            <h3 class="text-1xl md:text-2xl lg:text-4xl text-blue text-center pt-8">
+                6th of November 2025<br/>
+                LIEF, Amsterdam
+            </h3>
+            <div class="mt-12">
+                <a href="https://www.eventbrite.com/e/tickets-meet-magento-netherlands-2025-1325385617059"
+                   class="button button-big button-orange">
+                    Get your tickets
+                </a>
+            </div>
         </div>
-        <?= ImageFactory::create('headers/mmnl_x_cover_en.png', 'Meet Magento Nederland 2024')->setCssClass('object-cover w-full') ?>
-    </a>
+    </div>
 </div>
